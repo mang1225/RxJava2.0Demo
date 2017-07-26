2f944853247b9b963176841a4584294ae81c2213
LL117394 <okman999@aliyun.com>
2017-07-26 10:02:41 +0800

    Initial commit

---
+++ app/src/main/java/com/willkernel/www/rxjavademo/ui/TestActivity.java
@@ -0,0 +1,14 @@
+package com.willkernel.www.rxjavademo.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.willkernel.www.rxjavademo.R;
+
+public class TestActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_test);
+  }
+}
